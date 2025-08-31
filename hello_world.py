7ead1fde32c664dd5de6b2d497216ed83142d32a
RafaelFer77 <137837745+RafaelFer77@users.noreply.github.com>
2025-08-31 14:22:33 -0300

    hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,5 @@
+name = input("Whats your name? ")
+print("Hello, " + name + "!")
+age = input("How old are you? ")
+print("You are " + age + " years old.")
+print("Welcome to the world of programming, " + name + "!")
